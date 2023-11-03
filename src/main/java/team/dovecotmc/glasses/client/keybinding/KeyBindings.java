b7c44a71ecb94931de3e6064311e60a352cc4e5e
MikhailTapio <mikhailtapio13468@163.com>
2023-11-03 23:45:24 +0800

    Tiny fixes

--- src/main/java/team/dovecotmc/glasses/client/keybinding/KeyBindings.java
+++ src/main/java/team/dovecotmc/glasses/client/keybinding/KeyBindings.java
@@ -6,6 +6,6 @@ import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
 
 public class KeyBindings {
-    public static KeyMapping GLASSES_ACTION = new KeyMapping("key.glasses.glasses_action", KeyConflictContext.UNIVERSAL, KeyModifier.NONE,
-            InputConstants.Type.KEYSYM, InputConstants.KEY_X, "key.glasses.category");
+    public static KeyMapping GLASSES_ACTION = new KeyMapping("key.glasses.glasses_action", KeyConflictContext.IN_GAME, KeyModifier.NONE,
+            InputConstants.Type.KEYSYM, InputConstants.KEY_V, "key.glasses.category");
 }
