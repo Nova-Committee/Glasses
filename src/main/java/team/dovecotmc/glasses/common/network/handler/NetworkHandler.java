7984a444e004c08340e7e501e4f04717af643abd
MikhailTapio <mikhailtapio13468@163.com>
2023-11-03 13:15:19 +0800

    Remove useless VERSION field

--- src/main/java/team/dovecotmc/glasses/common/network/handler/NetworkHandler.java
+++ src/main/java/team/dovecotmc/glasses/common/network/handler/NetworkHandler.java
@@ -9,7 +9,6 @@ import team.dovecotmc.glasses.common.network.msg.GlassesUseMessage;
 
 public class NetworkHandler {
     public static SimpleChannel INSTANCE;
-    public static final String VERSION = "1.0";
     private static int id = 0;
 
     public static int nextId() {
