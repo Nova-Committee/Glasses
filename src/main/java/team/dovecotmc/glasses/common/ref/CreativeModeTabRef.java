bbd7a4bd224f3a6de65b0192f4c7271a192447c6
MikhailTapio <mikhailtapio13468@163.com>
2023-11-14 22:36:15 +0800

    Fix item order && keybinding logic

--- src/main/java/team/dovecotmc/glasses/common/ref/CreativeModeTabRef.java
+++ src/main/java/team/dovecotmc/glasses/common/ref/CreativeModeTabRef.java
@@ -5,10 +5,9 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import team.dovecotmc.glasses.Glasses;
-import team.dovecotmc.glasses.common.item.base.GlassesItem;
 
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.function.Supplier;
 
@@ -16,13 +15,7 @@ public enum CreativeModeTabRef implements Supplier<CreativeModeTab> {
     GLASSES(() -> CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
             .title(Component.translatable("itemGroup.glasses.glasses"))
             .icon(() -> ItemRef.SUNGLASSES_STYLE2.get().getDefaultInstance())
-            .displayItems((param, output) -> {
-                BuiltInRegistries.ITEM.entrySet().forEach(e -> {
-                    final Item i = e.getValue();
-                    if (!(i instanceof GlassesItem)) return;
-                    output.accept(i);
-                });
-            })
+            .displayItems((param, output) -> Arrays.stream(ItemRef.values()).forEach(r -> output.accept(r.get())))
             .build());
 
 
